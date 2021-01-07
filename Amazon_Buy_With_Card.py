87ebcc6aee54114141ff5aa3dbb1f0fc005a0075
datakund <54356773+datakund@users.noreply.github.com>
2021-01-07 15:21:27 +0530

    Update Amazon_Buy_With_Card.py

--- Amazon_Buy_With_Card.py
+++ Amazon_Buy_With_Card.py
@@ -1,6 +1,11 @@
 from amazon_auto_buy import *
+
 amazon.login_cookie(cookies=list_of_cookies)
+
 amazon.buy(product_url='product link')
+
 amazon.select_payment_method(payment_method='Punjab National Bank Debit Card')
+
 amazon.fill_cvv(cvv='cvv')
+
 amazon.place_order()
